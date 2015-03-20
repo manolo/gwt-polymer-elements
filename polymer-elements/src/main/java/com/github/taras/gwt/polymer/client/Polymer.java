dcc24257d209efd3ad5188649def4a93073268a5
Taras Hupalo <taras.hupalo@gmail.com>
2015-03-20 13:10:21 +0200

    removed hardcoded import map

--- polymer-elements/src/main/java/com/github/taras/gwt/polymer/client/Polymer.java
+++ polymer-elements/src/main/java/com/github/taras/gwt/polymer/client/Polymer.java
@@ -1,29 +1,19 @@
 package com.github.taras.gwt.polymer.client;
 
-import com.github.taras.gwt.polymer.client.elements.CoreField;
-import com.github.taras.gwt.polymer.client.elements.CoreLabel;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.taras.gwt.polymer.client.elements.ImportsMap;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class Polymer {
     
     private static Polymer instance = new Polymer();
 
     private static Set<String> urlImported = new HashSet<>();
-    
-    private static Map<String, String> importMap = new HashMap<>();
-    static {
-        importMap.put("core-field", "components/core-field/core-field.html");
-        importMap.put("core-label", "components/core-label/core-label.html");
-        importMap.put("paper-button", "components/paper-button/paper-button.html");
-    }
     
     public static <T extends PolymerElement> T create(String tagName) {
         Polymer.ensureHTMLImport(tagName);
@@ -34,7 +24,7 @@ public class Polymer {
 	 * Injects the correspondent html template to page head section.
 	 */
     public static void ensureHTMLImport(String tagName) {
-        String href = importMap.get(tagName);
+        String href = "components/" + ImportsMap.getInstance().get(tagName);
         if (!urlImported.contains(href)) {
             Element head = Document.get().getElementsByTagName("head").getItem(0);
             Element htmlImport = Document.get().createLinkElement();
