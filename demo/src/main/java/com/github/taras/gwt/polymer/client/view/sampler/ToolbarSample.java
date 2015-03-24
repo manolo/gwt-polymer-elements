83a9030588dc5feb57bfd4e22ca0271e1b524de3
Taras Hupalo <taras.hupalo@gmail.com>
2015-03-25 01:16:33 +0200

    added core-media-query listener

--- demo/src/main/java/com/github/taras/gwt/polymer/client/view/sampler/ToolbarSample.java
+++ demo/src/main/java/com/github/taras/gwt/polymer/client/view/sampler/ToolbarSample.java
@@ -1,20 +1,31 @@
 package com.github.taras.gwt.polymer.client.view.sampler;
 
+import com.github.taras.gwt.polymer.client.widget.CoreMediaQuery;
+import com.github.taras.gwt.polymer.client.widget.event.CoreMediaChangeEvent;
+import com.github.taras.gwt.polymer.client.widget.event.CoreMediaChangeEventHandler;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
-/**
- * Created by Taras on 24.03.2015.
- */
 public class ToolbarSample extends Composite {
     interface ToolbarSampleUiBinder extends UiBinder<HTMLPanel, ToolbarSample> {
     }
 
     private static ToolbarSampleUiBinder ourUiBinder = GWT.create(ToolbarSampleUiBinder.class);
+    @UiField
+    CoreMediaQuery mediaQuery;
 
     public ToolbarSample() {
         initWidget(ourUiBinder.createAndBindUi(this));
+        
+        mediaQuery.addCoreMediaChangeHandler(new CoreMediaChangeEventHandler() {
+            @Override
+            public void onCoreMediaChange(CoreMediaChangeEvent event) {
+                // TODO: investigate details of the CoreMediaChangeEvent
+//                if (event.getNativeEvent().getDetail().)
+            }
+        });
     }
 }
