33b6f01d55a49fb29545c65fc50299df4c0319e2
Manolo Carrasco <manolo@apache.org>
2015-05-24 11:48:54 -0700

    Refactor Sampler and introduce history hash

--- gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/Sampler.java
+++ gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/Sampler.java
@@ -1,16 +1,20 @@
 package com.vaadin.components.gwt.polymer.client.sampler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DeckPanel;
@@ -50,7 +54,7 @@ import com.vaadin.components.gwt.polymer.client.widget.event.CoreResponsiveChang
 
 public class Sampler extends Composite {
 
-    public static final String REPO_PATH = "https://github.com/cwayfinder/gwt-polymer/blob/master/demo/src/main/java/com/github/taras/gwt/polymer/client/sampler/";
+    public static final String REPO_PATH = "https://github.com/manolo/gwt-polymer/blob/mcm_fixes/gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/";
 
     interface SamplerUiBinder extends UiBinder<HTMLPanel, Sampler> {
     }
@@ -64,8 +68,9 @@ public class Sampler extends Composite {
     private static SamplerUiBinder ourUiBinder = GWT.create(SamplerUiBinder.class);
 
     private Map<String, CoreSelector> selectorMap = new HashMap<>();
-    private String path;
-    private Widget currentWidget;
+    private Map<String, CoreCollapse> collapseMap = new HashMap<>();
+    private List<Item> items = new ArrayList<>();
+    Item currentItem;
 
     @UiField Style style;
 
@@ -97,61 +102,65 @@ public class Sampler extends Composite {
         xmlButton.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                Window.open(REPO_PATH + path + ".ui.xml", "_blank", "");
+                Window.open(REPO_PATH + currentItem.category + "/" + currentItem.path + ".ui.xml", "_blank", "");
             }
         });
 
         javaButton.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent event) {
-                Window.open(REPO_PATH + path + ".java", "_blank", "");
+                Window.open(REPO_PATH + currentItem.category + "/" + currentItem.path + ".java", "_blank", "");
             }
         });
 
 
-        addCategory("Core Elements");
-        addSample("a11y keys", new A11yKeysSample(), "Core Elements", "core/A11yKeysSample");
-        addSample("Collapse", new CoreCollapseSample(), "Core Elements", "core/CoreCollapseSample");
+        addCategory("core", "Core Elements");
+        addSample("a11y keys", new A11yKeysSample(), "core", "A11yKeysSample");
+        addSample("Collapse", new CoreCollapseSample(), "core", "CoreCollapseSample");
 
-        addCategory("Paper Elements");
-        addSample("CheckBox", new CheckboxSample(), "Paper Elements", "paper/CheckboxSample");
-        addSample("Radio Button", new RadioButtonSample(), "Paper Elements", "paper/RadioButtonSample");
-        addSample("Toggle Button", new ToggleButtonSample(), "Paper Elements", "paper/ToggleButtonSample");
+        addCategory("paper", "Paper Elements");
+        addSample("CheckBox", new CheckboxSample(), "paper", "CheckboxSample");
+        addSample("Radio Button", new RadioButtonSample(), "paper", "RadioButtonSample");
+        addSample("Toggle Button", new ToggleButtonSample(), "paper", "ToggleButtonSample");
         // strange bug appears if include this to Sampler
-        addSample("Input", new InputSample(), "Paper Elements", "paper/InputSample");
-        addSample("Toolbar", new ToolbarSample(), "Paper Elements", "paper/ToolbarSample");
-        addSample("Spinner", new SpinnerSample(), "Paper Elements", "paper/SpinnerSample");
-        addSample("Progress Bar", new ProgressBarSample(), "Paper Elements", "paper/ProgressBarSample");
-        addSample("Slider", new SliderSample(), "Paper Elements", "paper/SliderSample");
-        addSample("Tabs", new TabsSample(), "Paper Elements", "paper/TabsSample");
-        addSample("Button", new ButtonSample(), "Paper Elements", "paper/ButtonSample");
-        addSample("Icon Button", new IconButtonSample(), "Paper Elements", "paper/IconButtonSample");
-        addSample("Floating Action Button", new FloatingActionButtonSample(), "Paper Elements", "paper/FloatingActionButtonSample");
+        addSample("Input", new InputSample(), "paper", "InputSample");
+        addSample("Toolbar", new ToolbarSample(), "paper", "ToolbarSample");
+        addSample("Spinner", new SpinnerSample(), "paper", "SpinnerSample");
+        addSample("Progress Bar", new ProgressBarSample(), "paper", "ProgressBarSample");
+        addSample("Slider", new SliderSample(), "paper", "SliderSample");
+        addSample("Tabs", new TabsSample(), "paper", "TabsSample");
+        addSample("Button", new ButtonSample(), "paper", "ButtonSample");
+        addSample("Icon Button", new IconButtonSample(), "paper", "IconButtonSample");
+        addSample("Floating Action Button", new FloatingActionButtonSample(), "paper", "FloatingActionButtonSample");
         // not fully implemented yet
-//        addSample("Dropdown", new DropdownSample(), "Paper Elements", "paper/DropdownSample");
-        addSample("Dropdown Menu", new DropdownMenuSample(), "Paper Elements", "paper/DropdownMenuSample");
-        addSample("Menu Button", new MenuButtonSample(), "Paper Elements", "paper/MenuButtonSample");
-        addSample("Item", new ItemSample(), "Paper Elements", "paper/ItemSample");
-        addSample("Dialog", new DialogSample(), "Paper Elements", "paper/DialogSample");
+//        addSample("Dropdown", new DropdownSample(), "paper", "DropdownSample");
+        addSample("Dropdown Menu", new DropdownMenuSample(), "paper", "DropdownMenuSample");
+        addSample("Menu Button", new MenuButtonSample(), "paper", "MenuButtonSample");
+        addSample("Item", new ItemSample(), "paper", "ItemSample");
+        addSample("Dialog", new DialogSample(), "paper", "DialogSample");
         // wait for pull-request, because setZ seems not working.
-        addSample("Shadow", new ShadowSample(), "Paper Elements", "paper/ShadowSample");
-        addSample("Toast", new ToastSample(), "Paper Elements", "paper/ToastSample");
+        addSample("Shadow", new ShadowSample(), "paper", "ShadowSample");
+        addSample("Toast", new ToastSample(), "paper", "ToastSample");
 
-        addCategory("Other");
-        addSample("JsInterop wrappers", new PaperTabsView(), "Other", "PaperTabsView");
-        addSample("Widget wrappers", new PaperTabsWidgetView(), "Other", "PaperTabsWidgetView");
+        addCategory("gwt", "Other");
+        addSample("JsInterop wrappers", new PaperTabsView(), "gwt", "PaperTabsView");
+        addSample("Widget wrappers", new PaperTabsWidgetView(), "gwt", "PaperTabsWidgetView");
 
-        selectRandomItem();
+        selectItem(Window.Location.getHash().replace("#", ""));
+
+        History.addValueChangeHandler(new ValueChangeHandler<String>() {
+            public void onValueChange(ValueChangeEvent<String> event) {
+                selectItem(event.getValue());
+            }
+        });
     }
 
-    private void addCategory(String name) {
+    private void addCategory(String path, String name) {
         PaperItem item = new PaperItem(name);
         item.addStyleName(style.category());
-
         final CoreCollapse collapse = new CoreCollapse("");
 
         item.addClickHandler(new ClickHandler() {
-            @Override
             public void onClick(ClickEvent event) {
                 collapse.toggle();
             }
@@ -159,40 +168,66 @@ public class Sampler extends Composite {
 
         CoreSelector selector = new CoreSelector("");
         collapse.add(selector);
-        selectorMap.put(name, selector);
+        selectorMap.put(path, selector);
+        collapseMap.put(path, collapse);
 
         listPanel.add(item);
         listPanel.add(collapse);
     }
 
-    private void addSample(final String name, final Widget sample, String categoryName, final String path) {
-        PaperItem item = new PaperItem(name);
-        item.addStyleName(style.item());
+    private void addSample(final String name, final Widget sample, String category, String path) {
         final int idx = content.getWidgetCount();
         content.add(sample);
 
-        item.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                currentLabel.setInnerText(name);
-                Sampler.this.path = path;
-                content.showWidget(idx);
-            }
-        });
-
-        CoreSelector selector = selectorMap.get(categoryName);
-        selector.add(item);
+        CoreCollapse collapse = collapseMap.get(category);
+        CoreSelector selector = selectorMap.get(category);
+        items.add(new Item(collapse, selector, category, path, idx, name));
     }
 
-    // TODO: Refactor this
-    private void selectRandomItem() {
-        (new Timer() {
-            @Override
-            public void run() {
-                ((CoreCollapse) listPanel.getWidget(3)).toggle();
-                selectorMap.get("Paper Elements").setSelected(0);
-                selectorMap.get("Paper Elements").getWidget(0).fireEvent(new ClickEvent() {});
+    private void selectItem(String hash) {
+        String tmp[] = hash != null ? hash.split("/") : new String[]{"paper", "CheckboxSample"};
+        if (tmp == null || tmp.length < 2) {
+            tmp = new String[]{"paper", "CheckboxSample"};
+        }
+        for (Item i : items) {
+            if (i.category == tmp[0] && i.path == tmp[1]) {
+                i.onClick(null);
+                return;
             }
-        }).schedule(500);
+        }
+    }
+
+    private class Item extends PaperItem implements ClickHandler {
+        String category;
+        CoreCollapse collapse;
+        CoreSelector selector;
+        String path;
+        String name;
+        int selectorIdx;
+        int sampleIdx;
+
+        public Item(CoreCollapse collapse, CoreSelector selector, String category, String path, int idx, String name) {
+            super(path);
+            this.collapse = collapse;
+            this.selector = selector;
+            this.category = category;
+            this.path = path;
+            this.name = name;
+            this.sampleIdx = idx;
+            selectorIdx = selector.getWidgetCount();
+            selector.add(this);
+            setName(name);
+            addStyleName(style.item());
+            addClickHandler(this);
+        }
+
+        public void onClick(ClickEvent event) {
+            collapse.setOpened(true);
+            selector.setSelected(selectorIdx);
+            content.showWidget(sampleIdx);
+            currentItem = this;
+            History.newItem(category + "/" + path, false);
+            currentLabel.setInnerText(name);
+        }
     }
 }
