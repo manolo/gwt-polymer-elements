bcfdfc0a3a20a9425d561c49102f310487414b7b
Manolo Carrasco <manolo@apache.org>
2015-05-26 08:18:54 -0700

    Use UiHandler in the example

--- gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/gwt/PaperTabsWidgetView.java
+++ gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/gwt/PaperTabsWidgetView.java
@@ -3,39 +3,35 @@ package com.vaadin.components.gwt.polymer.client.sampler.gwt;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.vaadin.components.gwt.polymer.client.widget.PaperTab;
 import com.vaadin.components.gwt.polymer.client.widget.PaperTabs;
 import com.vaadin.components.gwt.polymer.client.widget.PaperToast;
 import com.vaadin.components.gwt.polymer.client.widget.event.CoreSelectEvent;
-import com.vaadin.components.gwt.polymer.client.widget.event.CoreSelectEventHandler;
 
 public class PaperTabsWidgetView extends Composite {
     interface PaperTabsWidgetUiBinder extends UiBinder<HTMLPanel, PaperTabsWidgetView> {
     }
 
     private static PaperTabsWidgetUiBinder ourUiBinder = GWT.create(PaperTabsWidgetUiBinder.class);
-    
-    @UiField
-    PaperTabs tabs;
-    @UiField
-    PaperToast toast;
+
+    @UiField PaperTabs tabs;
+    @UiField PaperToast toast;
 
     public PaperTabsWidgetView() {
         initWidget(ourUiBinder.createAndBindUi(this));
-        
+
         tabs.setSelected(0);
 
         PaperTab tab = new PaperTab("dynamically created item");
         tabs.add(tab);
-        
-        tabs.addCoreSelectHandler(new CoreSelectEventHandler() {
-            @Override
-            public void onCoreSelect(CoreSelectEvent event) {
-                toast.setText("widget event handler");
-                toast.show();
-            }
-        });
+    }
+
+    @UiHandler("tabs")
+    void onNameGotPressed(CoreSelectEvent event) {
+        toast.setText("widget event handler");
+        toast.show();
     }
 }
