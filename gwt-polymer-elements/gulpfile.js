f45ee7ec34df4baaff32b84d33530ea5e3b79596
Manolo Carrasco <manolo@apache.org>
2015-06-04 10:29:32 +0200

    Refactor parsing so as we dont need temporary folders
    Also fixes problems serialiasing json when there are cyclic
    references.

--- gwt-polymer-elements/gulpfile.js
+++ gwt-polymer-elements/gulpfile.js
@@ -5,35 +5,29 @@ var rename = require('gulp-rename');
 var fs = require('fs-extra');
 var gutil = require('gulp-util');
 var _ = require('lodash');
-var ContextFreeParser = require("polymer-context-free-parser");
 var helpers = require("./template/helpers");
 var hyd = require("hydrolysis");
+var StreamFromArray = require('stream-from-array');
 
 var imports = [];
+var parsed = [];
+
+var ns = "/com/vaadin/components/gwt/polymer/";
+var namespace = "./src/main/java" + ns + "client/";
+var resources = "./src/main/resources" + ns + "public/";
 
 gulp.task('gwt-api:clean', function() {
-  fs.removeSync('src/main/java/com/vaadin/components/gwt/polymer/client/element');
-  fs.removeSync('src/main/java/com/vaadin/components/gwt/polymer/client/widget');
+  fs.removeSync(namespace + 'client/element');
+  fs.removeSync(namespace + 'client/widget');
 });
 
 gulp.task('clean', function() {
-  fs.removeSync('src/main/resources/com/vaadin/components/gwt/polymer/public');
+  fs.removeSync(namespace + 'public');
 });
 
-function serializeJson(path, item) {
-  helpers.removeDuplicates(item.properties, 'name');
-  helpers.removePrivateApi(item.properties, 'name');
-
-  fs.ensureFileSync(path);
-  fs.writeFileSync(path, new Buffer(JSON.stringify(item)));
-  // remember href in separate json
-  imports.push({
-    element: item.is,
-    path: file.relative.replace(/\\/, '/')
-  });
-}
 gulp.task('gwt-api:parse', ['gwt-api:clean'], function() {
-  var path = "src/main/resources/com/vaadin/components/gwt/polymer/public/bower_components/";
+  var path = resources + "bower_components/";
+
   return gulp.src([path + "*/*.html",
     "!" + path + "*/*demo.html",        // ignore all the demo.html files
     "!" + path + "*/*index.html",       // ignore all the index.html files
@@ -50,126 +44,78 @@ gulp.task('gwt-api:parse', ['gwt-api:clean'], function() {
           var path = dirname + item.is + '.json';
           if (item.is) {
             item.name = item.is;
-            gutil.log("Serializing " + path + " from: " + file.relative);
-            serializeJson(path, item);
-          } else {
-            // Use old parser
-            gutil.log("Using old parser -> " + item.name);
-            path = dirname + file.relative + '.json';
-            var jsonArray = ContextFreeParser.parse(file.contents.toString());
-            jsonArray.forEach(function(item) {
-              serializeJson(path, item);
-            })
+            // Hydrolysis repeat certain attributes
+            helpers.removeDuplicates(item.properties, 'name');
+            // We don't want to wrap private api
+            helpers.removePrivateApi(item.properties, 'name');
+            // Save all items in an array for later processing
+            parsed.push(item);
+            // remember href in separate java file
+            imports.push({
+              element: item.is,
+              path: file.relative.replace(/\\/, '/')
+            });
           }
         });
         cb(null, file);
       }).catch(function(e){
+        gutil.log(e.stack);
         cb(null, file);
       });
     }));
 });
 
+// Parse a template. It should be in the /template/ folder and have .template ext.
+// dir is relative to the namespace (gwt client) folder.
+function parseTemplate(template, obj, name, dir, suffix) {
+  var file = helpers.camelCase(name) + suffix;
+  var path = namespace + dir + file;
+  gutil.log("Generating: ", name, path);
+
+  var tpl = _.template(fs.readFileSync('./template/' + template + '.template'));
+  fs.ensureFileSync(path);
+  fs.writeFileSync(path, new Buffer(tpl(_.merge({}, null, obj, helpers))));
+}
+
 gulp.task('gwt-api:generate-elements', ['gwt-api:parse'], function() {
-  return gulp.src('dist-tmp/**.json')
-    .pipe(map(function(file, cb) {
-      gutil.log('Generating element interface from ' + file.relative);
-
-      var tpl = _.template(fs.readFileSync('./template/Interop.template'));
-      file.contents = new Buffer(tpl(_.merge({}, null, JSON.parse(file.contents), helpers)));
-
-      cb(null, file);
-    }))
-    .pipe(rename(function (file) {
-      file.basename = helpers.camelCase(file.basename) + 'Element';
-      file.extname = '.java';
-    }))
-    .pipe(gulp.dest('./src/main/java/com/vaadin/components/gwt/polymer/client/element'));
+  StreamFromArray(parsed,{objectMode: true})
+   .on('data', function(item) {
+      parseTemplate('Interop', item, item.is, 'element/', 'Element.java');
+   })
 });
 
 gulp.task('gwt-api:generate-events', ['gwt-api:parse'], function() {
-  var tpl = _.template(fs.readFileSync('./template/Event.template'));
-
-  return gulp.src('dist-tmp/**.json')
-    .pipe(map(function(file, cb) {
-      var json = JSON.parse(file.contents);
-      if (json.events) {
-        json.events.forEach(function(event) {
-          var content = tpl(_.merge({}, null, event, helpers));
-
-          // saves the result object as JSON
-          var dirname = 'src/main/java/com/vaadin/components/gwt/polymer/client/element/event/';
-          var filename = helpers.camelCase(event.name) + 'Event.java';
-          gutil.log('Generating ' + filename + ' from ' + file.relative);
-          var path = dirname + filename;
-          fs.ensureFileSync(path);
-          fs.writeFileSync(path, new Buffer(content));
+  StreamFromArray(parsed,{objectMode: true})
+   .on('data', function(item) {
+      if (item.events) {
+        item.events.forEach(function(event) {
+          parseTemplate('Event', event, event.name, 'element/event/', 'Event.java');
         });
       }
-
-      cb(null, file);
-    }));
+   })
 });
 
 gulp.task('gwt-api:generate-widgets', ['gwt-api:parse'], function() {
-  return gulp.src('dist-tmp/**.json')
-    .pipe(map(function(file, cb) {
-      gutil.log('Generating widget class from ' + file.relative);
-
-      var tpl = _.template(fs.readFileSync('./template/Widget.template'));
-      file.contents = new Buffer(tpl(_.merge({}, null, JSON.parse(file.contents), helpers)));
-
-      cb(null, file);
-    }))
-    .pipe(rename(function (file) {
-      file.basename = helpers.camelCase(file.basename);
-      file.extname = '.java';
-    }))
-    .pipe(gulp.dest('./src/main/java/com/vaadin/components/gwt/polymer/client/widget'));
+  StreamFromArray(parsed,{objectMode: true})
+   .on('data', function(item) {
+      parseTemplate('Widget', item, item.is, 'widget/', '.java');
+   })
 });
 
 gulp.task('gwt-api:generate-widget-events', ['gwt-api:parse'], function() {
-  var eventTpl = _.template(fs.readFileSync('./template/WidgetEvent.template'));
-  var handlerTpl = _.template(fs.readFileSync('./template/WidgetEventHandler.template'));
-
-  return gulp.src('dist-tmp/**.json')
-    .pipe(map(function(file, cb) {
-      var json = JSON.parse(file.contents);
-      if (json.events) {
-        json.events.forEach(function(event) {
-          var dirname = 'src/main/java/com/vaadin/components/gwt/polymer/client/widget/event/';
-          var filename = helpers.camelCase(event.name) + 'Event.java';
-          gutil.log('Generating ' + filename + ' from ' + file.relative);
-          var path = dirname + filename;
-          fs.ensureFileSync(path);
-          fs.writeFileSync(path, new Buffer(eventTpl(_.merge({}, null, event, helpers))));
-
-          dirname = 'src/main/java/com/vaadin/components/gwt/polymer/client/widget/event/';
-          filename = helpers.camelCase(event.name) + 'EventHandler.java';
-          gutil.log('Generating ' + filename + ' from ' + file.relative);
-          path = dirname + filename;
-          fs.ensureFileSync(path);
-          fs.writeFileSync(path, new Buffer(handlerTpl(_.merge({}, null, event, helpers))));
+  StreamFromArray(parsed,{objectMode: true})
+   .on('data', function(item) {
+      if (item.events) {
+        item.events.forEach(function(event) {
+          parseTemplate('WidgetEvent', event, event.name, 'widget/event/', 'Event.java');
+          parseTemplate('WidgetEventHandler', event, event.name, 'widget/event/', 'EventHandler.java');
         });
       }
-
-      cb(null, file);
-    }));
+   })
 });
 
 gulp.task('gwt-api:generate-imports-map', ['gwt-api:parse'], function() {
-  return gulp.src('./template/ImportsMap.template')
-    .pipe(map(function(file, cb) {
-      gutil.log('Generating ImportsMap class');
-
-      var tpl = _.template(file.contents);
-      file.contents = new Buffer(tpl(_.merge({}, null, {"imports": imports})));
-
-      cb(null, file);
-    }))
-    .pipe(rename(function (file) {
-      file.extname = '.java';
-    }))
-    .pipe(gulp.dest('./src/main/java/com/vaadin/components/gwt/polymer/client/element'));
+  parseTemplate('ImportsMap', {"imports" : imports}, 'imports-map', 'element/', '.java');
 });
 
 gulp.task('gwt-api', ['gwt-api:generate-elements', 'gwt-api:generate-events', 'gwt-api:generate-widgets', 'gwt-api:generate-widget-events', 'gwt-api:generate-imports-map']);
