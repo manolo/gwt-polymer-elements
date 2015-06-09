3bcf3d8496b129140ac69d654bf2b926dfd61bd5
Taras Hupalo <taras.hupalo@gmail.com>
2015-06-09 21:00:58 +0300

    Fix after merge

--- gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/paper/SpinnerSample.java
+++ gwt-polymer-demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/paper/SpinnerSample.java
@@ -1,10 +1,13 @@
 package com.vaadin.components.gwt.polymer.client.sampler.paper;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
@@ -29,62 +32,42 @@ public class SpinnerSample extends Composite {
     @UiField PaperSpinner toggle2_1;
     @UiField PaperSpinner toggle2_2;
     @UiField PaperSpinner toggle2_3;
-    @UiField PaperSpinner toggle2_4;
+    @UiField PaperSpinnerElement toggle2_4;
 
     public SpinnerSample() {
-        Polymer.ensureHTMLImport("paper-spinner/paper-spinner.html");
-
         initWidget(ourUiBinder.createAndBindUi(SpinnerSample.this));
-
-        toggle1_1.getPolymerElement().setAttribute("active", "");
-        toggle1_2.getPolymerElement().setAttribute("active", "");
-        toggle1_3.getPolymerElement().setAttribute("active", "");
-        toggle1_4.getPolymerElement().setAttribute("active", "");
-
-        toggle2_1.getPolymerElement().setAttribute("active", "");
-        toggle2_2.getPolymerElement().setAttribute("active", "");
-        toggle2_3.getPolymerElement().setAttribute("active", "");
-        toggle2_4.getPolymerElement().setAttribute("active", "");
-
-        toggle2_1.getPolymerElement().setAttribute("class", "blue");
-        toggle2_2.getPolymerElement().setAttribute("class", "red");
-        toggle2_3.getPolymerElement().setAttribute("class", "orange");
-        toggle2_4.getPolymerElement().setAttribute("class", "green");
 
         toggleBtn1.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent clickEvent) {
-                PaperSpinnerElement el = toggle1_1.getPolymerElement();
-                if (el.hasAttribute("active")) {
-                    el.removeAttribute("active");
-                    toggle1_2.getPolymerElement().removeAttribute("active");
-                    toggle1_3.getPolymerElement().removeAttribute("active");
-                    toggle1_4.getPolymerElement().removeAttribute("active");
-                } else {
-                    el.setAttribute("active", "");
-                    toggle1_2.getPolymerElement().setAttribute("active", "");
-                    toggle1_3.getPolymerElement().setAttribute("active", "");
-                    toggle1_4.getPolymerElement().setAttribute("active", "");
-                }
+                toggleAttribute(toggle1_1);
+                toggleAttribute(toggle1_2);
+                toggleAttribute(toggle1_3);
+                toggleAttribute(toggle1_4);
             }
         });
 
         toggleBtn2.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent clickEvent) {
-                PaperSpinnerElement el = toggle2_1.getPolymerElement();
-                if (el.hasAttribute("active")) {
-                    el.removeAttribute("active");
-                    toggle2_2.getPolymerElement().removeAttribute("active");
-                    toggle2_3.getPolymerElement().removeAttribute("active");
-                    toggle2_4.getPolymerElement().removeAttribute("active");
-                } else {
-                    el.setAttribute("active", "");
-                    toggle2_2.getPolymerElement().setAttribute("active", "");
-                    toggle2_3.getPolymerElement().setAttribute("active", "");
-                    toggle2_4.getPolymerElement().setAttribute("active", "");
-                }
+                toggleAttribute(toggle2_1);
+                toggleAttribute(toggle2_2);
+                toggleAttribute(toggle2_3);
+                toggleAttribute(toggle2_4);
             }
         });
+    }
+
+    private void toggleAttribute(PaperSpinner spinner) {
+        toggleAttribute(spinner.getPolymerElement());
+    }
+
+    private void toggleAttribute(PaperSpinnerElement element) {
+        String name = "active";
+        if (element.hasAttribute(name)) {
+            element.removeAttribute(name);
+        } else {
+            element.setAttribute(name, "");
+        }
     }
 }
