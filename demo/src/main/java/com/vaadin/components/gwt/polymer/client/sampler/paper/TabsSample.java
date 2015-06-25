9f79ea3a5a1835fdd4914ef8b9e635fd7e2b39fc
Manolo Carrasco <manolo@apache.org>
2015-06-25 11:42:29 +0200

    Move workaround to a Base class so it works not only in example

--- demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/paper/TabsSample.java
+++ demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/paper/TabsSample.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.vaadin.polymer.Polymer;
 
 public class TabsSample extends Composite {
     interface SampleUiBinder extends UiBinder<HTMLPanel, TabsSample> {
@@ -15,23 +14,4 @@ public class TabsSample extends Composite {
     public TabsSample() {
         initWidget(ourUiBinder.createAndBindUi(this));
     }
-
-    @Override
-    protected void onAttach() {
-        super.onAttach();
-
-        fixTabsSelection();
-    }
-
-    // A temporary workaround to fix the underline tab issue
-    // Not a solution.
-    private native void fixTabsSelection() /*-{
-
-      setTimeout(function(){
-
-        [].forEach.call($doc.querySelectorAll("paper-tabs"), function(tabs) {
-          tabs._onResize();
-        });
-      }, 3000);
-    }-*/;
 }
