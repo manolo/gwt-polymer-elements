ea46c47a2cdd106616e500d11f783b48dd1b2a67
Manolo Carrasco <manolo@apache.org>
2015-09-09 22:07:32 +0200

    the "View source bar" was not visible in IronList example

--- demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/iron/IronListSample.java
+++ demo/src/main/java/com/vaadin/components/gwt/polymer/client/sampler/iron/IronListSample.java
@@ -4,15 +4,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.vaadin.polymer.Polymer;
 import com.vaadin.polymer.iron.widget.IronAjax;
-import com.vaadin.polymer.iron.widget.IronImage;
 import com.vaadin.polymer.iron.widget.IronList;
-import com.vaadin.polymer.response.widget.event.ResponseEvent;
-import com.vaadin.polymer.response.widget.event.ResponseEventHandler;
 
 public class IronListSample extends Composite {
     interface SampleUiBinder extends UiBinder<HTMLPanel, IronListSample> {
@@ -20,15 +15,12 @@ public class IronListSample extends Composite {
 
     private static SampleUiBinder ourUiBinder = GWT.create(SampleUiBinder.class);
 
-    @UiField
-    IronAjax ajax;
-
-    @UiField
-    IronList list;
-
+    @UiField IronAjax ajax;
+    @UiField IronList list;
+    
     public IronListSample() {
         initWidget(ourUiBinder.createAndBindUi(this));
 
-        ajax.addResponseHandler(event -> list.setItems((JsArray) ajax.getLastResponse()));
+        ajax.addResponseHandler(event -> list.setItems((JsArray<?>) ajax.getLastResponse()));
     }
 }
