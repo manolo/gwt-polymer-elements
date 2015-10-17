1846df8c2beef4a28e80479aed103464ac3c0dc3
Manolo Carrasco <manolo@apache.org>
2015-10-17 18:12:39 +0200

    Use HtmlImport instead of timer hack

--- demo/src/main/java/com/vaadin/polymer/demo/client/Demo.java
+++ demo/src/main/java/com/vaadin/polymer/demo/client/Demo.java
@@ -14,31 +14,30 @@ public class Demo implements EntryPoint {
         Polymer.startLoading();
 
         // Although gwt-polymer-elements takes care of dynamic loading of components
-        // if they are created using Polymer.createElement or Polymer Widgets,
+        // when they are created using Polymer.createElement or we use Polymer Widgets,
         // there are certain features which must be loaded previously to start
         // the application. Hence you have to add import tags to your host page or
-        // import those additional features dynamically, in this case it might be
-        // necessary to wait until the components are ready.
+        // import them dynamically, in this case it might be necessary to wait until
+        // the components are ready.
+
         // The `Polymer` utility class provide a set of methods for facilitating it,
         // you can pass tag-names for standard component locations (tag-name/tag-name.html)
-        // or relative urls otherwise. Also you can pass success and error call-backs.
-
-        // Paper applications must always import paper-styles
-        Polymer.importHref("paper-styles");
-
-        // Styles for paper examples
-        Polymer.importHref("paper-styles/demo-pages.html");
-
-        // - Iconsets must be loaded before using any component depending on them
-        //   Iron comes with the collections:
-        //   communication, device, editor, hardware, image, iron, maps, notifications, social.
-        // - Animation must be loaded at the beginning as wll
+        // or relative urls otherwise.
         Polymer.importHref(Arrays.asList(
+                // Paper applications must always import paper-styles
+                "paper-styles",
+                // Styles for paper examples
+                "paper-styles/demo-pages.html",
+                // Iconsets must be loaded before using any component depending on them
+                //   Iron comes with the collections:
+                //   communication, device, editor, hardware, image, iron, maps, notifications, social.
                 "iron-icons/iron-icons.html",
                 "iron-icons/communication-icons.html",
                 "iron-flex-layout/iron-flex-layout.html",
+                // Animation must be loaded at the beginning
                 "neon-animation/neon-animations.html"
-                ), new Function() {
+                ));
+        Polymer.whenReady(new Function() {
             public Object call(Object arg) {
                 // The app is executed when all imports succeed.
                 RootPanel.get().add(new Sampler());
