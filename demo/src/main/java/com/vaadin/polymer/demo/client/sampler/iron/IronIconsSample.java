18b646f99f7dd0745d25a895fbd4cc83fe38bd5e
Manolo Carrasco <manolo@apache.org>
2016-01-18 08:55:55 +0100

    Adding IronIcons and VaadinIcons demos

---
+++ demo/src/main/java/com/vaadin/polymer/demo/client/sampler/iron/IronIconsSample.java
@@ -0,0 +1,27 @@
+package com.vaadin.polymer.demo.client.sampler.iron;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.elemental.Function;
+import com.vaadin.polymer.elemental.Template;
+
+public class IronIconsSample extends Composite {
+    interface SampleUiBinder extends UiBinder<HTMLPanel, IronIconsSample> {
+    }
+
+    private static SampleUiBinder ourUiBinder = GWT.create(SampleUiBinder.class);
+    
+    @UiField Template bindTemplate;
+
+    public IronIconsSample() {
+        initWidget(ourUiBinder.createAndBindUi(this));
+
+        // Example of how to bind functions to custom elements. 
+        Polymer.property(bindTemplate, "getIconNames",
+                (Function<?,?>) iconSet -> Polymer.apply(iconSet, "getIconNames"));
+    }
+}
