1f82a1427da87865109b7ad35c1446d3da532f0e
Manolo Carrasco <manolo@apache.org>
2016-01-18 08:55:27 +0100

    Adding IronAjax example

---
+++ demo/src/main/java/com/vaadin/polymer/demo/client/sampler/iron/IronAjaxSample.java
@@ -0,0 +1,31 @@
+package com.vaadin.polymer.demo.client.sampler.iron;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.elemental.Template;
+import com.vaadin.polymer.iron.widget.IronAjax;
+import com.vaadin.polymer.iron.widget.event.ResponseEvent;
+
+public class IronAjaxSample extends Composite {
+    interface SampleUiBinder extends UiBinder<HTMLPanel, IronAjaxSample> {
+    }
+
+    private static SampleUiBinder ourUiBinder = GWT.create(SampleUiBinder.class);
+
+    @UiField IronAjax ajax;
+    @UiField Template repeat;
+
+    public IronAjaxSample() {
+        initWidget(ourUiBinder.createAndBindUi(this));
+    }
+    
+    @UiHandler("ajax")
+    void onResponse(ResponseEvent e) {
+        repeat.setItems(Polymer.property(ajax.getLastResponse(), "items"));
+    }
+}
